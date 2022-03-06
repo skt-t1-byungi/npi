e496ee6cff5a34e3d83fe01a0e9e982cec1d8e36
byungi <zeronin2@gmail.com>
2022-03-07 03:24:37 +0900

    wip

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,23 @@
 import sade from 'sade'
-import pkg from '../package.json'
+import path from 'node:path'
+import fs from 'node:fs'
+import pkgJson from '../package.json'
+import { detect } from 'detect-package-manager'
+import pack from 'libnpmpack'
+import execa from 'execa'
 
 sade('npi [package]', true)
-    .version(pkg.version)
+    .version(pkgJson.version)
     .option('-b, --bin', 'Package Manager binary.')
-    .option('-d, --dev', 'Install to devDependencies.')
-    .option('-p, --peer', 'Install to peerDependencies.')
+    .option('-D, --dev', 'Install to devDependencies.')
+    .option('-P, --peer', 'Install to peerDependencies.')
     .option('-f, --folder', 'Folder to be installed. Default is "vendors".', 'vendors')
-    .action((pkg: string, opts: any) => () => {
-        console.log(pkg, opts)
+    .action(async (pkg: string, { bin, dev, peer, folder }: Record<string, string>) => {
+        const cwd = process.cwd()
+        const dest = path.resolve(cwd, folder)
+
+        fs.mkdirSync(dest, { recursive: true })
+        const [pm, tarball] = await Promise.all([bin ?? detect({ cwd }), pack(pkg)])
+        fs.writeFileSync(path.resolve(dest, path.basename(tarball.resolved)), tarball)
     })
     .parse(process.argv)
