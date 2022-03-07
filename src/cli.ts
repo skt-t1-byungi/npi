440a1b323c397399c7ef960e0ce381ebdd00e482
byungi <zeronin2@gmail.com>
2022-03-07 12:25:15 +0900

    wip

--- src/cli.ts
+++ src/cli.ts
@@ -1,23 +1,47 @@
 import sade from 'sade'
 import path from 'node:path'
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 import pkgJson from '../package.json'
 import { detect } from 'detect-package-manager'
 import pack from 'libnpmpack'
-import execa from 'execa'
+import { execa } from 'execa'
 
-sade('npi [package]', true)
+sade('npi <package>', true)
     .version(pkgJson.version)
-    .option('-b, --bin', 'Package Manager binary.')
+    .option('-i, --installer', 'Package manager for install.')
+    .option('-d, --dir', 'Directory to download. Default is "vendors".', 'vendors')
     .option('-D, --dev', 'Install to devDependencies.')
     .option('-P, --peer', 'Install to peerDependencies.')
-    .option('-f, --folder', 'Folder to be installed. Default is "vendors".', 'vendors')
-    .action(async (pkg: string, { bin, dev, peer, folder }: Record<string, string>) => {
-        const cwd = process.cwd()
-        const dest = path.resolve(cwd, folder)
+    .option('-O, --optional', 'Install to optionalDependencies.')
+    .action(
+        async (pkg: string, { _, installer, dir, dev, peer, optional }: Record<string, string> & { _: string[] }) => {
+            const cwd = process.cwd()
 
-        fs.mkdirSync(dest, { recursive: true })
-        const [pm, tarball] = await Promise.all([bin ?? detect({ cwd }), pack(pkg)])
-        fs.writeFileSync(path.resolve(dest, path.basename(tarball.resolved)), tarball)
-    })
+            installer ??= await detect({ cwd })
+            if (!['npm', 'yarn', 'pnpm'].includes(installer)) {
+                throw new Error(`Unsupported installer: ${installer}`)
+            }
+
+            await fs.mkdir((dir = path.resolve(cwd, dir)), { recursive: true })
+
+            const packages = _.concat(pkg)
+            const files = await Promise.all(
+                packages.map(async pkg => {
+                    const tarball = await pack(pkg)
+                    const file = path.resolve(dir, path.basename(tarball.resolved))
+                    await fs.writeFile(file, tarball)
+                    return './' + path.relative(cwd, file).replaceAll('\\', '/')
+                })
+            )
+
+            const mode = dev ? 'dev' : peer ? 'peer' : optional ? 'optional' : null
+            await execa(
+                installer,
+                ['add', ...files, mode && `${installer === 'yarn' ? '--' : '--save-'}${mode}`].filter(
+                    Boolean
+                ) as string[],
+                { cwd, stdout: process.stdout, stderr: process.stderr }
+            )
+        }
+    )
     .parse(process.argv)
